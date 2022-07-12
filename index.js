0cca3815f1b02025463cea2c8abac9f007977baf
Argomaru <48087677+Argomaru@users.noreply.github.com>
2022-07-12 09:22:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,138 @@
+var nama = "Jason";
+var name = "kristian";
+
+let 
+//type data variable
+//type data data string, number, boolean, array, function
+//number dibagi 2 ada integer dan float
+//aturan: ga bisa pake spasi, cuman bisa pake rules letter, underscore, dollar sign
+//case sensitive
+
+// var _nama = "Jason";
+// var $nama = "Jason";
+// var Nama = "Jason";
+// let angka =string(10);
+// angka = Boolean(10);
+//console.log(nama);
+//console.log('test');
+//menampilkan login dari isinya
+
+//console.log(typeof nama);
+//var nmb = '23'
+//var name = "jason";
+//var nmb1 = 123
+
+//console.log(typeof nmb1);
+
+//type of = "funct untuk nentuin isi dari suatu data itu jenisnya apaan"
+
+//console.log(nmb+ nmb1)
+//hasilnya bakal nyatu jadi 123123
+
+//console.log(nmb-nmb1)
+//bakal jadi pengurangan biaa tapi ganti data type ke string
+
+//console.log(nama-nmb)
+//hasilnya jadi NaN
+
+//console.log(nama*nmb)
+//hasilnya jadi NaN
+
+//console.log(nama/nmb)
+//hasilnya jadi NaN
+
+//modulus adalah sisa dari pembagian (hasilnya paling mendekati)
+// var angka = 20
+//var angka1 = 5
+//console.log(angka%angka1)
+//hasilnya 0
+
+//var angka = 23
+//var angka1 = 3
+//console.log(angka%angka1)
+//hasilnya 2
+//karena perkalian 3 yang paling mendekati 23 adalah 3*7 = 21 dan sisanya mendekati 2
+
+//increment = nambah value dari vaiable jadi 1
+//var angka = 5
+//angka++
+//console.log(angka)
+//hasilnya jadi 6
+
+//decrement = ngurang value dari vaiable jadi 1
+//var angka = 5
+//angka++
+//console.log(angka)
+//hasilnya jadi 4
+
+//shorthand operator
+//var number = 4
+//var number1 = 5
+//bisa menggunakan
+// number = number + number1 "atau" number += number1
+//console.log(number)
+//hasilnya jadi 9
+
+//var score = 0 //mulainya 0 nih
+//score += 10 // 0+10 = 10
+//score *= 2 // 10*2 = 20
+//score /= 5 // 20/5 = 4
+//hasilnya 4
+//console.log(score)
+
+//math object(basic)
+//Math.PI //pi = 3.141592653589793
+//Math.abs(-4.7) //absolute = jadi positif
+//Math.pow(9, 2) //pemangkatan = 81
+//Math.sqrt(81) //akar dari 81 hasilnya = 9
+//Math.cbrt(8) // cuberoot/akar pangkat 3 dari 8 hasilnya = 2
+//contoh penulisannya
+//console.log(Math.cbrt(125))
+//console.log(Math.PI)
+//console.log(Math.abs(-121))
+//dll
+
+//Math Methods(Round, Floor, Ceil)
+//Math.Round //rounding angka decimal
+//Math.Floor //pembulatan ke yang lebih rendah
+//Math.Ciel //pembulatan ke yang lebih tinggi
+
+//var random = Math.random() //0-1 (defaultnya)
+//console.log(random)
+//hailnya dari 0 - 1 (bisa aja 0.blabla)
+
+//Parsing Data Types (ubah tipe data tanap operasi math)
+//String
+//var stringangka = '123'
+//console.log(stringangka) //hasilnya tetep string
+//console.log(parseInt(stringangka)) //hasilnya berubah jadi int
+
+//var intangka = 123
+//console.log(intangka.toString()) //hasilnya berubah jadi string
+
+//hello \n guys (sebagai enter)
+//hello \t guys (sebagai tab)
+//console.log("hello \ti'm \tthere") //hailnya jadi = hello   i'm     there
+
+//String with Backtick
+//var stringnumber = "123"
+//console.log("test angka" + stringnumber)
+//test angka123
+
+//String Properties & Methods
+//var x = "Hello World";
+//itungan length dimulai dari 0
+//console.log(x.length); //total karakter dari string
+//hasilnya 11
+
+//console.log(x.indexOf("World")); //mencari urutan ke berapa dari karakter
+//hasilnya 6
+
+//console.log(x.substring(5, 3)); //ngambil string berdasarkan karakter ke berapa
+//hasilnya lo
+
+//console.log(x.slice(5, 8)); //ekstract bagian dari index
+//hasilnya  Wo
+
+//console.log(x.split(' ')); //di pisah
+//hasilnya [ 'Hello', 'World' ]
